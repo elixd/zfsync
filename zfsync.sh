2e9c5ba0589e5c61844efa869a0266f597fe8c89
elixd <57058175+elixd@users.noreply.github.com>
2019-10-29 20:24:40 +0300

    Update zfsync.sh

--- zfsync.sh
+++ zfsync.sh
@@ -7,7 +7,7 @@ SRC=$1
 DST=$2
 
 # CREATE NEW SNAPSHOT OF SOURCE FILESYSTEM
-CURRENT_TIME=$(date +%Y-%m-%d-%H%M%S)
+CURRENT_TIME=$(date +%Y-%m-%d-%H%M)
 echo "MAKING SNAPSHOT:"
 echo "zfs snapshot -r "$SRC@$CURRENT_TIME
 zfs snapshot -r $SRC@$CURRENT_TIME
@@ -26,4 +26,13 @@ echo "zfs send -R -i "$SNAP1" " $SNAP2" | pv | zfs receive -F "$DST
 zfs send -R -i $SNAP1  $SNAP2 | pv | zfs receive -F $DST
 
 
+
+###### NOTES ######
+# Delete snapshots recursively
+# zfs list -t snapshot -o name -S creation | grep ^tank@Auto | tail -n +16 | xargs -n 1 zfs destroy -vr
+# https://serverfault.com/questions/340837/how-to-delete-all-but-last-n-zfs-snapshots
+# 
+# Even better - destoroy a range of snapshots
+# zfs destroy tank@snap1%snap200 # snap1 is older than snap200 
+# zfs destroy tank@2019-10-28-120800%2019-10-29-184402
 
